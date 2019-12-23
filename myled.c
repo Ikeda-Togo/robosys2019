79608d84d2529f4dbe1b046f612b42e8056b9566
IkedaTogo <47003257+Ikeda-Togo@users.noreply.github.com>
2019-12-24 01:01:17 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -4,6 +4,9 @@
 #include<linux/device.h>
 #include<linux/uaccess.h>
 #include<linux/io.h>
+#include <asm/delay.h>
+#include <asm/uaccess.h>
+
 
 MODULE_AUTHOR("Ryuichi Ueda");
 MODULE_DESCRIPTION("drinver for LED control");
@@ -14,28 +17,77 @@ static struct cdev cdv;
 static struct class *cls=NULL;
 static volatile u32 *gpio_base =NULL;
 
+
 static ssize_t led_write(struct file* filp,const char* buf,size_t count,loff_t* pos)
 {
 	char c;
+	int i,j;
+	int k=100;
+
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
 
-	if(c=='0')
-		gpio_base[10]=1<<25;
-	else if(c=='1')
-		gpio_base[7]=1<<25;
+	if(c=='l'){
+		while(1){
+		for(i=22;i<=25;i++){
+			gpio_base[10]=1<<22;
+			gpio_base[10]=1<<23;
+			gpio_base[10]=1<<24;
+			gpio_base[10]=1<<25;
+			gpio_base[7]=1<<i;
+			k+=100;
+			for(j=0; j < k; j++){
+			udelay(500);}
+			}
+		if(k>2000){
+		break;}
+		}
+	}	
 
-	return 1;
+
+	if(c=='r'){
+		while(1){
+		for(i=25;i>=22;i--){
+			gpio_base[10]=1<<22;
+			gpio_base[10]=1<<23;
+			gpio_base[10]=1<<24;
+			gpio_base[10]=1<<25;
+			gpio_base[7]=1<<i;
+			k+=100;
+			for(j=0; j < k; j++){
+			udelay(500);}
+			}
+		if(k>2000){
+		break;}
+		}
+	}	
+
+	else if(c=='1'){
+		gpio_base[7]=1<<22;
+		gpio_base[7]=1<<23;
+		gpio_base[7]=1<<24;
+		gpio_base[7]=1<<25;
+			
+		for(j=0; j < 1000; j++){
+		udelay(500);}
+		
+		gpio_base[10]=1<<22;
+		gpio_base[10]=1<<23;
+		gpio_base[10]=1<<24;
+		gpio_base[10]=1<<25;
+	}	
+	
 
 	printk(KERN_INFO"receive %c\n",c);
 	return 1; //読み込んだ文字数を返す（この場合はダミーの1）
 }
 
+
+
 static struct file_operations led_fops = {
 	. owner=THIS_MODULE,
 	. write=led_write
 };
-
 
 static int __init init_mod(void)
 {
@@ -43,11 +95,30 @@ static int __init init_mod(void)
 	int retval;
 	gpio_base=ioremap_nocache(0x3f200000,0xA0);
 
+
 	const u32 led = 25;
 	const u32 index = led/10;
 	const u32 shift = (led%10)*3;
 	const u32 mask = ~(0x7 << shift);
 	gpio_base[index]=(gpio_base[index]&mask) | (0x1<<shift);
+
+	const u32 led2 = 22;
+	const u32 index2 = led2/10;//GPFSEL2
+	const u32 shift2 = (led2%10)*3;//15bit
+	const u32 mask2 = ~(0x7 << shift2);//11111111111111000111111111111111
+	gpio_base[index] = (gpio_base[index] & mask2) | (0x1 << shift2);//001: output flag
+
+	const u32 led3 = 23;
+	const u32 index3 = led3/10;//GPFSEL2
+	const u32 shift3 = (led3%10)*3;//15bit
+	const u32 mask3 = ~(0x7 << shift3);//11111111111111000111111111111111
+	gpio_base[index] = (gpio_base[index] & mask3) | (0x1 << shift3);//001: output flag
+
+	const u32 led4 = 24;
+	const u32 index4 = led4/10;//GPFSEL2
+	const u32 shift4 = (led4%10)*3;//15bit
+	const u32 mask4 = ~(0x7 << shift4);//11111111111111000111111111111111
+	gpio_base[index] = (gpio_base[index] & mask4) | (0x1 << shift4);//001: output flag
 
 	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
 	if (retval<0){
@@ -57,6 +128,7 @@ static int __init init_mod(void)
 	printk(KERN_INFO"%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
 	cdev_init(&cdv,&led_fops);
+	cdv.owner=THIS_MODULE;
 	retval =cdev_add(&cdv,dev,1);
 	if(retval<0){
 		printk(KERN_ERR"cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
@@ -78,6 +150,7 @@ static void __exit cleanup_mod(void)//後始末
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO"%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+	iounmap(gpio_base);
 }
 
 module_init(init_mod); //マクロ関数を登録
